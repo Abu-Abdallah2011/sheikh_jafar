5720a611523efe81aa2de96cc5cae57f035060ab
SADIQ MUSTAPHA AHMAD <sadiqmustaphaahmad@gmail.com>
2024-12-05 00:56:25 -0800

    rectify teachers database and single teacher

--- resources/views/single_teacher.blade.php
+++ resources/views/single_teacher.blade.php
@@ -10,7 +10,7 @@
                 <div class="flex">
                     <div class="">
                         <div class="grid grid-flow-col col-md-6 text-right">
-                            @if ($student->photo)
+                            @if ($teacher->photo)
                             <img class="w-48 mr-6 md:block" src="{{ Storage::disk('s3')->url($teacher->photo) }}" alt="" />
                             @endif
                         </div>
